dcdf42a2db502b9fc9a9d71571911081ca0cc757
Liam Appelbe <liamappelbe@gmail.com>
2022-04-10 18:14:15 -0700

    Add windowing functions

--- test/generate_fft_test_cases.py
+++ test/generate_fft_test_cases.py
@@ -19,10 +19,10 @@ import random
 kNumsPerLine = 4
 
 def randReal(r):
-  return random.uniform(-r, r);
+  return random.uniform(-r, r)
 
 def randCplx(r):
-  return complex(randReal(r), randReal(r));
+  return complex(randReal(r), randReal(r))
 
 def realBufStr(a):
   def impl(b):
@@ -48,14 +48,14 @@ def cplxBufStr(a):
   return realBufStr(b)
 
 def makeFftCase(n):
-  a = [randCplx(10) for i in range(n)];
+  a = [randCplx(10) for i in range(n)]
   f = numpy.fft.fft(a)
   print('    testFft(')
   print('      [%s],' % cplxBufStr(a))
   print('      [%s],' % cplxBufStr(f))
   print('    );')
 
-print("  test('FFT', () {");
+print("  test('FFT', () {")
 makeFftCase(1)
 makeFftCase(2)
 makeFftCase(4)
@@ -64,17 +64,17 @@ makeFftCase(16)
 makeFftCase(32)
 makeFftCase(64)
 makeFftCase(128)
+print('  });\n')
 
 def makeRealFftCase(n):
-  a = [randReal(10) for i in range(n)];
+  a = [randReal(10) for i in range(n)]
   f = numpy.fft.fft(a)
   print('    testRealFft(')
   print('      [%s],' % realBufStr(a))
   print('      [%s],' % cplxBufStr(f))
   print('    );')
 
-print('  });\n')
-print("  test('Real FFT', () {");
+print("  test('Real FFT', () {")
 makeRealFftCase(1)
 makeRealFftCase(2)
 makeRealFftCase(4)
@@ -83,4 +83,38 @@ makeRealFftCase(16)
 makeRealFftCase(32)
 makeRealFftCase(64)
 makeRealFftCase(128)
+print('  });\n')
+
+def makeWindowCase(n, name, fn):
+  print('    expectClose(')
+  print('      Window.%s(%s).array,' % (name, n))
+  print('      [%s],' % realBufStr(fn(n)))
+  print('    );')
+
+print("  test('Window', () {")
+makeWindowCase(16, 'hamming', numpy.hamming)
+makeWindowCase(16, 'hanning', numpy.hanning)
+makeWindowCase(16, 'bartlett', numpy.bartlett)
+makeWindowCase(16, 'blackman', numpy.blackman)
+print('  });\n')
+
+def makeWindowApplyCase(n):
+  a = [randReal(10) for i in range(n)]
+  b = numpy.hamming(n) * a
+  c = [randCplx(10) for i in range(n)]
+  d = numpy.hamming(n) * c
+  print('    final w = Window.hamming(16);')
+  print('    final a = [%s];' % realBufStr(a))
+  print('    expectClose(')
+  print('      w.applyReal(Float64List.fromList(a)),')
+  print('      [%s],' % realBufStr(b))
+  print('    );');
+  print('    final b = [%s];' % cplxBufStr(c))
+  print('    expectClose(')
+  print('      w.apply(ComplexArray(Float64List.fromList(b))).array,')
+  print('      [%s],' % cplxBufStr(d))
+  print('    );');
+
+print("  test('Window apply', () {")
+makeWindowApplyCase(16)
 print('  });')
